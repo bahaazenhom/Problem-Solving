267568d5d7351dab08891b50efcf753d1c32a000
Bahaa Zenhom <40721733+bahaazenhom@users.noreply.github.com>
2022-07-11 22:36:10 +0200

    Create PairClass.java

---
+++ PairClass.java
@@ -0,0 +1,43 @@
+class Pair implements Comparable<Pair> {
+    long left;
+    long right;
+
+    public Pair(long left,long right) {
+        this.left=left;
+        this.right=right;
+    }
+
+    public long getLeft() {
+        return this.left;
+    }
+
+    public long getRight() {
+        return this.right;
+    }
+
+    public void setLeft(long left) {
+        this.left=left;
+    }
+    public void setRight(long right) {
+        this.right=right;
+    }
+
+    public static Pair[] sort(Pair[] pairs) {
+        Arrays.sort(pairs);
+        return pairs;
+    }
+
+    @Override
+    public int compareTo(Pair pair) {
+        if (pair.getLeft() > this.getLeft()) return -1;
+        else if (pair.getLeft() < this.getLeft()) return 1;
+        else return 0;
+    }
+
+    public static void toString(Pair[] pairs) {
+        for (int i = 0; i < pairs.length; i++) {
+            System.out.print(pairs[i].getLeft() +" "+pairs[i].getRight());
+            System.out.println();
+        }
+    }
+}
