bc417d4d1a9d68ee31a387362fa94bc8e9609e45
Bahaa Zenhom <40721733+bahaazenhom@users.noreply.github.com>
2022-08-10 01:28:28 +0200

    Create Mutli TreeSet.java

---
+++ Create Multi TreeSet or Multi TreeMap in java/Mutli TreeSet.java
@@ -0,0 +1,6 @@
+TreeSet<E> treeSet=new TreeSet<>(new Comparator<E>() {
+           @Override
+           public int compare(E o1, E o2) {
+              // return o1>o2?1:-1; make your comparison here and make sure to return only 1 or -1 even in equal cases
+           }
+       });
